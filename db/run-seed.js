9938466f09279f401375fce4bb959aacf154b462
“Hayp” <hanna.y.peters@gmail.com>
2025-03-20 13:51:06 +0000

    setup

--- db/run-seed.js
+++ db/run-seed.js
@@ -1,6 +1,7 @@
 const seed = require("./seed");
 const db = require("./connection");
+const test_scores = require("./data/test_scores");
 
-seed().then(() => {
+seed(test_scores).then(() => {
   return db.end();
 });
--- db/seed.js
+++ db/seed.js
@@ -1,19 +1,30 @@
 const db = require("./connection");
 const format = require("pg-format");
+const test_scores = require("./data/test_scores");
 
-function seed() {
+function seed(test_scores) {
   return db
     .query("DROP TABLE IF EXISTS scores;")
-
     .then(() => {
       return createScoreTable();
+    })
+    .then(() => {
+      return insertScoreData(test_scores);
     });
 }
 
 function createScoreTable() {
-  return db.query(`CREATE TABLE SCORES(
+  return db.query(`CREATE TABLE scores(
         score INT
         )`);
+}
+
+function insertScoreData(test_scores) {
+  const query = format(
+    `INSERT INTO scores (score) VALUES %L;`,
+    test_scores.map((s) => [s])
+  );
+  return db.query(query);
 }
 
 module.exports = seed;
