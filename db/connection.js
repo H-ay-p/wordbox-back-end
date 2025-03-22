477fb07392c3a6935eb9335d66a403a4cc1407be
“Hayp” <hanna.y.peters@gmail.com>
2025-03-22 20:48:55 +0000

    supabase setup

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,21 @@
 const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
+// if (!process.env.PGDATABASE) {
+//   throw new Error("No PGDATABASE configured");
+// }
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
+
+// module.exports = new Pool();
