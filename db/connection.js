ad2db94c64c7a77c8a1e812e911721ac22f050bb
“Hayp” <hanna.y.peters@gmail.com>
2025-03-23 15:05:13 +0000

    update connection

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,10 @@ const ENV = process.env.NODE_ENV || "development";
 //   throw new Error("No PGDATABASE configured");
 // }
 
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
