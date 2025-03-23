1ef59b1dafd1b30cce04307c58a0f61b0f245f9b
“Hayp” <hanna.y.peters@gmail.com>
2025-03-23 18:18:54 +0000

    fix cors

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const postScore = require("./controllers/postScoreController.js");
 const getLetters = require("./functions/getLettersFunc.js");
 const cors = require("cors");
 
-app.use(cors());
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/healthcheck", (req, res) => {
